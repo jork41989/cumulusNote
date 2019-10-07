537a08c37369b6caeb6e17bad22820a99ecb4b22
Jordan Ackerman <jork41989@gmail.com>
2019-10-07 17:15:23 -0400

    added half of front end User auth

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,27 @@
+class Api::SessionsController < ApplicationController
+
+  def create
+    
+    @user = User.find_by_credentials( params[:user][:username], params[:user][:password])
+
+    if @user
+      login!(@user)
+      render 'api/users/show'
+    else
+      render json: ["Failed to log in"], status: 401
+    end
+  end
+
+  def destroy
+    @user = current_user
+    if @user #Render an empty {} upon successful logout.
+      logout!
+      render json: {}
+    else #Render a 404 message if there is no current_user to logout.
+      render json: ["No current user"], status: 404
+    end
+
+  end
+
+
+end
---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,33 @@
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
+
+import * as Util from `../util/session_api_util`
+
+const receiverCurrentUser = user => ({
+  type: RECEIVE_CURRENT_USER,
+  user
+});
+
+const logoutCurrentUser = () => ({
+  type: LOGOUT_CURRENT_USER
+});
+
+const receiveErrors = errors => ({
+  type: RECEIVE_SESSION_ERRORS,
+  errors
+});
+
+export const signup = user => dispatch => {
+  return Util.signup(user).then(user => dispatch(receiverCurrentUser(user)),
+  errors => dispatch(receiveErrors(errors.responseJSON)) );
+};
+
+export const login = user => dispatch => {
+  return Util.login(user).then(user => dispatch(receiverCurrentUser(user)),
+    errors => dispatch(receiveErrors(errors.responseJSON)));
+}
+
+export const logout = () => dispatch => {
+  return Util.logout().then( () => dispatch(logoutCurrentUser()))
+}
