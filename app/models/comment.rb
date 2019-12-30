afd03612cee7d36eef6623aa3d6ceb0c7f9a5026
Jordan Ackerman <jork41989@gmail.com>
2019-12-30 16:07:06 -0500

    Comments on the comments model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -11,11 +11,17 @@
 #  updated_at     :datetime         not null
 #
 
+# A comment has a body, a user_id (for ownership), a (possible) parent comment, a song_id (the song where the comment was created), and time stamps for creation and updates.
 class Comment < ApplicationRecord
   validates :body, presence: true
+  # Makes sure there is a body before saving
   validates :user_id, presence: true
+  # Makes sure there is a user_id (owner) attached before saving.
   validates :song_id, presence: true
+  # Makes sure there is a song_id (The song where the comment belongs) attached before saving.
 
   belongs_to :user, class_name: "User", foreign_key: :user_id
+  # Association for user who created the comment.
   belongs_to :song, class_name: "Song", foreign_key: :song_id
+  # Association for the song where the comment should be displayed.
 end
